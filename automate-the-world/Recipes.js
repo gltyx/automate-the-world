853b11532f8b789e1660aa769f2f64d4e35acd27
zhaolinxu <zhaolinxu@sina.cn>
2023-05-04 16:06:21 +0800

    hh

---
+++ automate-the-world/Recipes.js
@@ -0,0 +1,54 @@
+let recipes = [
+  {
+    name: 'Iron Gear',
+    itemName: 'ironGear',
+    requirements: [
+      {
+        material: 'ironPlate',
+        materialName: 'iron plate',
+        amount: 2
+      }
+    ]
+  },
+  {
+    name: 'Copper Coil',
+    itemName: 'copperCoil',
+    requirements: [
+      {
+        material: 'copperPlate',
+        materialName: 'copper plate',
+        amount: 2
+      }
+    ]
+  },
+  {
+    name: 'Red Science',
+    itemName: 'redScience',
+    requirements: [
+      {
+        material: 'copperPlate',
+        materialName: 'copper plate',
+        amount: 1
+      }, {
+        material: 'ironPlate',
+        materialName: 'iron plate',
+        amount: 1
+      }
+    ]
+  },
+  {
+    name: 'Basic Circuit',
+    itemName: 'basicCircuit',
+    requirements: [
+      {
+        material: 'ironPlate',
+        materialName: 'iron plate',
+        amount: 1
+      }, {
+        material: 'copperCoil',
+        materialName: 'copper coil',
+        amount: 2
+      }
+    ]
+  }
+]
